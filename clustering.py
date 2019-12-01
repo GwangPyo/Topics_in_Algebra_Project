754454119016ec409979ccb624e10ae4c6f62857
GwangPyo <31953954+GwangPyo@users.noreply.github.com>
2019-12-01 12:05:22 +0900

    clean_up, add vae

--- clustering.py
+++ clustering.py
@@ -1,2 +1,22 @@
 import sklearn
 import numpy as np
+from utils import sampling
+
+
+
+if __name__ == "__main__":
+    light = np.load("lights.npy")
+    xdata = np.load("X_data.npy")
+    tenper = len(xdata) // 10
+    unlabeled, labeled = sampling(len(xdata), tenper)
+
+    labeled_data = xdata[labeled, :]
+    label = light[labeled, :]
+
+    for i in range(4):
+        label_feature = np.zeros(4)
+        label_feature[i] = 1
+        label_index = np.where(label == label_feature)
+        print(labeled_data[label_index])
+
+
